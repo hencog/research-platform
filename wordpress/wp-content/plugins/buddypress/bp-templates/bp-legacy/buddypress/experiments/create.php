c3c5a63b6293a7217da00e50ca4faba7cbcd3dc1
Umar Rashid <umer.rashid.mir@gmail.com>
2014-06-06 16:18:19 +0100

    create.php updated

--- wordpress/wp-content/plugins/buddypress/bp-templates/bp-legacy/buddypress/experiments/create.php
+++ wordpress/wp-content/plugins/buddypress/bp-templates/bp-legacy/buddypress/experiments/create.php
@@ -4,8 +4,8 @@
 
 <!-- Optionally use Animate.css -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.0.0/animate.min.css">
-<!--link rel="stylesheet" href="http://digitalbrain-test.lancs.ac.uk/liquidslider/css/liquid-slider.css"-->
-<link rel="stylesheet" href="http://localhost/liquidslider/css/liquid-slider.css">
+<link rel="stylesheet" href="http://digitalbrain-test.lancs.ac.uk/liquidslider/css/liquid-slider.css">
+<!--link rel="stylesheet" href="http://localhost/liquidslider/css/liquid-slider.css"-->
 
 <!--link rel="stylesheet" href="http://localhost/wordpress/liquidslider/examples/assets/prism.css"-->
 <!--link rel="stylesheet" href="http://digitalbrain-test.lancs.ac.uk/liquidslider/examples/assets/styles.css"-->
@@ -426,8 +426,8 @@ function removeVariable() {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <!--script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.touchswipe/1.6.4/jquery.touchSwipe.min.js"></script>
-<!--script src="http://digitalbrain-test.lancs.ac.uk/liquidslider/js/jquery.liquid-slider.min.js"></script-->
-<script src="http://localhost/liquidslider/js/jquery.liquid-slider.min.js"></script>
+<script src="http://digitalbrain-test.lancs.ac.uk/liquidslider/js/jquery.liquid-slider.min.js"></script>
+<!--script src="http://localhost/liquidslider/js/jquery.liquid-slider.min.js"></script-->
 <script>
 /**
  * If you need to access the internal property or methods, use this:
